51153548fcf0afa6e089e570804c06152d5c946c
Estudio de moda <practicantelisto@estudiodemoda.com.co>
2025-10-02 15:52:05 -0500

    a

--- src/Services/Festivos.ts
+++ src/Services/Festivos.ts
@@ -1,12 +1,7 @@
-export async function fetchHolidays(country = "CO"): Promise<string[]> {
+import holidaysColombia from "festivos-colombianos";
+
+export async function fetchHolidays(): Promise<string[]> {
   const year = new Date().getFullYear(); //Obtener el año actual
-  console.log(country, " ", year)
-  const url = `https://api.generadordni.es/v2/holidays/holidays?country=${country}&year=${year}`;
-
-  const res = await fetch(url);
-  if (!res.ok) throw new Error("Error obteniendo festivos");
-  const data = await res.json();
-
-  // La API devuelve { date: "2025-01-01", localName: "..." } → normalizamos
-  return data.map((h: any) => h.date); // array ["2025-01-01", "2025-05-01", ...]
+  const holidays = holidaysColombia(year);
+  return holidays.map((h: any) => h.holidays); // array ["2025-01-01", "2025-05-01", ...]
 }
