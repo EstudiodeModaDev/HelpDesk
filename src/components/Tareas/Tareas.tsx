5bff68f48238490a330c0881a3ecf3bb74c515af
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2025-10-17 08:40:59 -0500

    Cambios visuales tareas

--- src/components/Tareas/Tareas.tsx
+++ src/components/Tareas/Tareas.tsx
@@ -1,18 +1,11 @@
 import FormTarea from "./TareasForm/TareasForm";
 import ListaTareas from "../Tareas/TareasRegistradas/TareasRegistradas";
+import "./TareasPage.css"
 
 export default function TareasPage() {
 
   return (
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: "min(520px, 38vw) 1fr",
-        gap: 24,
-        alignItems: "start",
-        padding: 16,
-      }}
-    >
+    <div className="tareas-page">
       <FormTarea />
       <ListaTareas/>
     </div>
