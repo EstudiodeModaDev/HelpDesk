38a92777151d748b3512b9254504b942a8acb930
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2025-10-28 10:33:24 -0500

    Tercera parte tareas 2

--- src/components/Tareas/Tareas.tsx
+++ src/components/Tareas/Tareas.tsx
@@ -2,14 +2,21 @@ import FormTarea from "./TareasForm/TareasForm";
 import ListaTareas from "../Tareas/TareasRegistradas/TareasRegistradas";
 import "./TareasPage.css"
 import ActivityStatusCard from "./ResumenActividad/ResumenActividad";
+import { useGraphServices } from "../../graph/GrapServicesContext";
+import type { TareasService } from "../../Services/Tareas.service";
+import { useTareas } from "../../Funcionalidades/Tareas";
 
 export default function TareasPage() {
 
+  const {Tareas} = (useGraphServices() as ReturnType<typeof useGraphServices> & {
+    Tareas: TareasService
+  });
+  const { monthlyItems, percentaje } = useTareas(Tareas);
   return (
     <div className="tareas-page">
       <FormTarea />
       <ListaTareas/>
-      <ActivityStatusCard percent={80} tasks={[]} tasksPerWeek={0}/>
+      <ActivityStatusCard percent={percentaje} tasks={monthlyItems} tasksPerWeek={0}/>
     </div>
   );
 }
