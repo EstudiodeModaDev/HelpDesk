a954f33668e9752b79b3fee8d9a4e208998106f8
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2025-10-30 16:15:18 -0500

    Attachments en ticket 11

--- src/Funcionalidades/AttachmentsTickets.ts
+++ src/Funcionalidades/AttachmentsTickets.ts
@@ -7,7 +7,7 @@ export function useTicketsAttachments(TicketsSvc: TicketsService, id: string) {
   const [rows, setRows] = React.useState<AttachmentLite[]>([]);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState<string | null>(null);
-   const notifyFlow = new FlowClient("https://defaultcd48ecd97e154f4b97d9ec813ee42b.2c.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/ceb573986da649129d18d563480129eb/triggers/manual/paths/invoke?api-version=1")
+   const notifyFlow = new FlowClient("https://defaultcd48ecd97e154f4b97d9ec813ee42b.2c.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/ceb573986da649129d18d563480129eb/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FSUJmCm8sGHZGVlNA6xD1bRyn2gFiIVXmsW53CbDAHM")
 
   const loadAttchments = React.useCallback(async () => {
     setLoading(true); setError(null);
